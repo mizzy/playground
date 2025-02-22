d4b7fef629043f6df2b0a3b65bd3bf71c96a4065
Gosuke Miyashita <gosukenator@gmail.com>
2025-02-22 19:51:41 +0900

    Encrypt Datadog API key

--- fargate-with-fluentbit/terraform/datadog.tf
+++ fargate-with-fluentbit/terraform/datadog.tf
@@ -1,9 +1,19 @@
+resource "aws_kms_key" "datadog" {}
+
+resource "aws_kms_alias" "datadog" {
+  name          = "alias/datadog"
+  target_key_id = aws_kms_key.datadog.id
+}
+
+data "aws_kms_secrets" "datadog" {
+  secret {
+    name    = "api-key"
+    payload = "AQICAHgJlo4J0kaIE5OgiI8JGp+qNuTdj/xLvbAg0sRlvX2XtQEkgYzlYAlBiD/XjGG3Iwl7AAAAfjB8BgkqhkiG9w0BBwagbzBtAgEAMGgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMDHaysRE0GNDAmeWuAgEQgDu0EG0p9au8s85SJVQFiTnekJy6zPRXGDQVQCW0Zdwi63USc+sky/Zc9aW/+N6fNGE39kSBFecC4qWxoA=="
+  }
+}
+
 resource "aws_ssm_parameter" "datadog_api_key" {
   name  = "/dev/datadog_api_key"
   type  = "SecureString"
-  value = "dummy"
-
-  lifecycle {
-    ignore_changes = [value]
-  }
+  value = data.aws_kms_secrets.datadog.plaintext["api-key"]
 }
