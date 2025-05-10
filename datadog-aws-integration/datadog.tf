adef509eaf0001a0ccb6b7ca9dd26ba5ac3a522d
Gosuke Miyashita <gosukenator@gmail.com>
2025-05-10 17:53:02 +0900

    Add Datadog monitor

--- datadog-aws-integration/datadog.tf
+++ datadog-aws-integration/datadog.tf
@@ -1,5 +1,5 @@
 resource "datadog_integration_aws_account" "foo" {
-  account_tags = ["env:prod"]
+  account_tags   = ["env:prod"]
   aws_account_id = data.aws_caller_identity.current.account_id
   aws_partition  = "aws"
 
@@ -38,5 +38,24 @@ resource "datadog_integration_aws_account" "foo" {
   logs_config {
     lambda_forwarder {
     }
+  }
+}
+
+data "aws_servicequotas_service_quota" "dynamodb_table_count" {
+  service_code = "dynamodb"
+  quota_name   = "Maximum number of tables"
+}
+
+resource "datadog_monitor" "dynamodb_table_count_quota" {
+  include_tags        = true
+  message             = "@slack-mizzy-test"
+  name                = "DynamoDB Table Count Quota"
+  on_missing_data     = "default"
+  query               = "max(last_5m):avg:aws.usage.resource_count.maximum{resource:tablecount} / ${data.aws_servicequotas_service_quota.dynamodb_table_count.value} * 100 > 80"
+  type                = "query alert"
+  require_full_window = false
+
+  monitor_thresholds {
+    critical = "80"
   }
 }
