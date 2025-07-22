3cc9675fdf759a2bb4f232293770414d788d1b16
Gosuke Miyashita <gosukenator@gmail.com>
2025-07-22 23:25:56 +0900

    Update iam

--- workload-identity-with-aws-ecs-tasks/terraform/iam.tf
+++ workload-identity-with-aws-ecs-tasks/terraform/iam.tf
@@ -11,19 +11,6 @@ resource "google_service_account_iam_binding" "workload_identity_user" {
   service_account_id = google_service_account.spreadsheet_service_account.name
   role               = "roles/iam.workloadIdentityUser"
   members = [
-    // "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.mizzy.name}/attribute.aws_role/${data.aws_iam_role.task_role.name}",
-    "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.mizzy.name}/*",
+    "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.mizzy.name}/attribute.aws_role/${data.aws_iam_role.task_role.name}",
   ]
 }
-
-resource "google_service_account_iam_binding" "impersonation_binding_with_data" {
-  service_account_id = google_service_account.spreadsheet_service_account.name
-  role               = "roles/iam.serviceAccountTokenCreator"
-  members = [
-    "user:gosukenator@gmail.com",
-    "serviceAccount:terraform-serviceaccount@mizzy-270104.iam.gserviceaccount.com",
-    //"principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.mizzy.name}/attribute.aws_role/${data.aws_iam_role.task_role.name}",
-    "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.mizzy.name}/*",
-  ]
-}
-
