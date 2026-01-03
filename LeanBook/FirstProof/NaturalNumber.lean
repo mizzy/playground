b5e1c397fb9129acec25f0ae39b096c5eb20d2c2
Gosuke Miyashita <gosukenator@gmail.com>
2026-01-03 15:36:54 +0900

    Lean Book Chapter 2

---
+++ LeanBook/FirstProof/NaturalNumber.lean
@@ -0,0 +1,27 @@
+inductive MyNat where
+  | zero
+  | succ (n : MyNat)
+
+#check MyNat.zero
+
+#check MyNat.succ .zero
+
+def MyNat.one := MyNat.succ .zero
+
+def MyNat.two := MyNat.succ .one
+
+def MyNat.add (m n : MyNat) : MyNat :=
+  match n with
+  | .zero => m
+  | .succ n => succ (add m n )
+
+#check MyNat.add .one .one = .two
+
+set_option pp.fieldNotation.generalized false
+
+#reduce MyNat.add .one .one
+
+#reduce MyNat.two
+
+example : MyNat.add .one .one = .two := by
+  rfl
